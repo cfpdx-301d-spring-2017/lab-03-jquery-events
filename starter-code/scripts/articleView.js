5b935c709c6e07622d41725f68b05a0c99506552
Joe Churchman <churchman.joe@gmail.com>
2017-05-31 15:24:04 -0700

    finished lab MVP

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -100,7 +100,12 @@ articleView.setTeasers = function () {
   //       "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   //       Ideally, we'd attach this as just 1 event handler on the #articles section, and let it
   //       process any .read-on clicks that happen within child nodes.
+  $('#articles').on('click', '.read-on', function () {
+    event.preventDefault();
+    $('.read-on').hide();
+    $('.article-body *:nth-of-type(n+2)').fadeIn(750);
 
+  })
   // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
 
 };
@@ -111,4 +116,5 @@ $(document).ready(function () {
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
+  articleView.setTeasers();
 })
