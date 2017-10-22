3729faa6873d49266ffc699d7ce506d725217e99
J3ffcon1 <Jrlone1990@gmail.com>
2017-10-22 15:09:47 -0700

    added script tag, and show/hid articles

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,6 +40,10 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      // $Article('#author-filter').hide();
+    $('article').hide();
+    var selectedAuthor = $(this).val();
+    $('article[data-author ="' +  selectedAuthor +'"]').show();
 
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
@@ -84,5 +88,7 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
 
 })
